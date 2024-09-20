c0e27546e8e139260dae5dc089aaf87064adb6ab
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-09-20 18:37:59 +0700

    adding section houses

--- src/components/section/houses/Houses.tsx
+++ src/components/section/houses/Houses.tsx
@@ -1,10 +1,23 @@
 import Slider from "react-slick";
+import { housesPoduct } from "../../../constants/constants";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import "../../../index.css";
+
 export const Houses = () => {
-  const settings = {};
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: false,
+    speed: 1500,
+    autoplaySpeed: 2000,
+    adaptiveHeight: true,
+    pauseOnHover: true,
+  };
 
   return (
     <div className="w-12/12 px-[150px] my-[70px]">
@@ -13,7 +26,37 @@ export const Houses = () => {
       </h1>
 
       <div>
-        <Slider {...settings}></Slider>
+        <Slider {...settings}>
+          {housesPoduct.map((item) => (
+            <div
+              key={item.id}
+              className="content-item relative overflow-hidden  lg:h-[230px] 2xl:h-[330px]
+          "
+            >
+              {item.special && item.option && (
+                <div>
+                  <span className="absolute font-semibold top-0 2xl:w-[180px] lg:w-[150px] z-10 right-0 px-2 py-1 text-center text-white bg-bannerSpecial rounded-bl-2xl rounded-tr-2xl ">
+                    {item.special}{" "}
+                    <span className="font-extralight">{item.option}</span>
+                  </span>
+                </div>
+              )}
+              <img
+                src={item.img}
+                alt={item.tittle}
+                className="object-cover w-full h-180 rounded-2xl"
+              />
+              <div className=" z-10 pb-4 text-white left-4">
+                <span className="text-lg font-normal text-secondColor 2xl:text-2xl">
+                  {item.tittle}
+                </span>
+                <p className="text-secondColor font-extralight 2xl:text-xl">
+                  {item.city}
+                </p>
+              </div>
+            </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
--- src/constants/constants.tsx
+++ src/constants/constants.tsx
@@ -14,6 +14,15 @@ import mostPick3 from "../assets/img/mostpick-3.jpg";
 import mostPick4 from "../assets/img/mostpick-4.jpg";
 import mostPick5 from "../assets/img/mostpick-5.jpg";
 
+// IMAGE HOUSE
+import houseImg from "../assets/img/houses/houses-1.jpg";
+import houseImg2 from "../assets/img/houses/houses-2.jpg";
+import houseImg3 from "../assets/img/houses/houses-3.jpg";
+import houseImg4 from "../assets/img/houses/houses-4.jpg";
+import houseImg5 from "../assets/img/houses/houses-5.jpg";
+import houseImg6 from "../assets/img/houses/houses-6.jpg";
+import houseImg7 from "../assets/img/houses/houses-7.jpg";
+
 export const navbarItems: navbarItem[] = [
   {
     label: "Home",
@@ -125,6 +134,67 @@ export const itemProductSmall: itemProduct[] = [
     city: "Medan, Indonesia",
     special: "$72",
     option: "per night",
+    description:
+      "Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.",
+  },
+];
+
+export const housesPoduct: itemProduct[] = [
+  {
+    id: 1,
+    img: houseImg,
+    tittle: "Toby Town House",
+    city: "Gunung Batu, Indonesia",
+    special: "Popular",
+    option: "Choice",
+    description:
+      "Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.",
+  },
+  {
+    id: 2,
+    img: houseImg2,
+    tittle: "Aggana",
+    city: "Bogor, Indonesia",
+    description:
+      "Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.",
+  },
+  {
+    id: 3,
+    img: houseImg3,
+    tittle: "Seattle Rain",
+    city: "Jakarta, Indonesia",
+    description:
+      "Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.",
+  },
+  {
+    id: 4,
+    img: houseImg4,
+    tittle: "Wodden Pit",
+    city: "Wonosobo, Indonesia",
+    description:
+      "Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.",
+  },
+  {
+    id: 5,
+    img: houseImg5,
+    tittle: "Harry House",
+    city: "Malang, Indonesia",
+    description:
+      "Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.",
+  },
+  {
+    id: 6,
+    img: houseImg6,
+    tittle: "Luxury House",
+    city: "Bandung, Indonesia",
+    description:
+      "Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.",
+  },
+  {
+    id: 7,
+    img: houseImg7,
+    tittle: "Stark House",
+    city: "Malang, Indonesia",
     description:
       "Minimal techno is a minimalist subgenre of techno music. It is characterized by a stripped-down aesthetic that exploits the use of repetition and understated development. Minimal techno is thought to have been originally developed in the early 1990s by Detroit-based producers Robert Hood and Daniel Bell.",
   },
