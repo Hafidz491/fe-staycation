a898a6603b06bb4d0c35c97566c08e08794e4f72
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-09-23 22:52:40 +0700

    add responsive for screen size

--- src/components/story/Story.tsx
+++ src/components/story/Story.tsx
@@ -4,15 +4,15 @@ import { Button } from "../button/Button";
 
 export const Story = () => {
   return (
-    <div className="flex gap-14 px-[150px] my-[70px]">
+    <div className="flex 2xl:justify-center 2xl:items-center gap-14 px-[150px] my-[70px]">
       <div className="w-full h-full">
-        <img src={foto} alt="photos family" />
+        <img src={foto} alt="photos family" className="2xl:mx-auto" />
       </div>
       <div className="flex flex-col w-full justify-center">
         <h1 className="text-2xl font-medium text-secondColor">Happy Family</h1>
         <div className=" mt-[40px]">
           <img src={rates} alt="rates" className="w-[180px] h-[36px]" />
-          <p className="text-[24px] font-light text-secondColor mt-[8px]">
+          <p className="text-[24px] 2xl:text-4xl font-light text-secondColor mt-[8px]">
             What a great trip with my family and I should try again next time
             soon...
           </p>
