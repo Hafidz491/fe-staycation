272b6168e77de51e06d1f05acbc99117c378099a
hafidz491 <hafidzhidayatulloh491@gmail.com>
2024-09-23 22:24:24 +0700

    adding story section

--- src/components/story/Story.tsx
+++ src/components/story/Story.tsx
@@ -1,13 +1,26 @@
 import foto from "../../assets/img/foto_keluarga.jpg";
+import rates from "../../assets/img/rate.png";
+import { Button } from "../button/Button";
 
 export const Story = () => {
   return (
-    <div className="flex justify-center">
-      <div className="">
-        <img src={foto} alt="" />
+    <div className="flex gap-14 px-[150px] my-[70px]">
+      <div className="w-full h-full">
+        <img src={foto} alt="photos family" />
       </div>
-      <div>
-        <h1>Story Teller</h1>
+      <div className="flex flex-col w-full justify-center">
+        <h1 className="text-2xl font-medium text-secondColor">Happy Family</h1>
+        <div className=" mt-[40px]">
+          <img src={rates} alt="rates" className="w-[180px] h-[36px]" />
+          <p className="text-[24px] font-light text-secondColor mt-[8px]">
+            What a great trip with my family and I should try again next time
+            soon...
+          </p>
+          <p className="text-[18px] font-light text-thirdColor mt-[8px]">
+            Angga, Product Manager
+          </p>
+        </div>
+        <Button variant="w-[210px] h-[50px] mt-[50px]">Read Their Story</Button>
       </div>
     </div>
   );
